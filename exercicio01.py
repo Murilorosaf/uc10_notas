37a17fd06732dee724d27479d770b1d72457ead7
geralt345 <guilhermevargasrodrigues8@gmail.com>
2023-11-01 15:04:10 -0300

    codigo de notas

---
+++ exercicio01.py
@@ -0,0 +1,14 @@
+print("exercicio de github")
+
+nome = input("digite seu nome: ")
+
+print(f"o nome digitado foi: {nome}")
+
+nota1 = int (input("digite sua primeira nota: "))
+nota2 = int (input("digite sua primeira nota: "))
+nota3 = int (input("digite sua primeira nota: "))
+nota4 = int (input("digite sua primeira nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4) /4
+print(f"sua media foi {media}")
+
